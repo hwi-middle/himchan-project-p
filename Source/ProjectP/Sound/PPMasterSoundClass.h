1268a5618b02bb3fd094c188980fa24f2bb24c8c
Cr0ssEyE <hangman5166@gmail.com>
2023-07-28 23:47:27 +0900

    [#39] SoundClass 추가

---
+++ Source/ProjectP/Sound/PPMasterSoundClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sound/SoundClass.h"
+#include "PPMasterSoundClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTP_API UPPMasterSoundClass : public USoundClass
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectP/Sound/PPSFXSoundClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectP/Sound/PPMasterSoundClass.h"
+#include "PPSFXSoundClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTP_API UPPSFXSoundClass : public UPPMasterSoundClass
+{
+	GENERATED_BODY()
+	
+};
